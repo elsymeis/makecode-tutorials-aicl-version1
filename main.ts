71c046a85efc27b13702879353652206fbd38ba6
elsymeis <56837582+elsymeis@users.noreply.github.com>
2021-06-02 15:48:54 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,1 @@
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        basic.showIcon(IconNames.Happy)
-    } else if (input.buttonIsPressed(Button.B)) {
-        basic.showIcon(IconNames.Sad)
-    } else {
-        basic.showIcon(IconNames.Asleep)
-    }
-})
+
