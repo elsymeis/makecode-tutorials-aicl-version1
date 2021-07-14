6757b098b5b8036f4c76de03d0e87bb84dbc53e5
elsymeis <56837582+elsymeis@users.noreply.github.com>
2021-07-14 12:24:56 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,1 @@
-let strip: neopixel.Strip = null
-input.onButtonPressed(Button.B, function () {
-    basic.showString("" + (gatorEnvironment.getMeasurement(measurementType.eCO2)))
-    strip = neopixel.create(DigitalPin.P12, 24, NeoPixelMode.RGB)
-    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-})
+
